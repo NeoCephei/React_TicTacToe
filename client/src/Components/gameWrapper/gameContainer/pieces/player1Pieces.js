9f23e9c9f83869ae0c4e7a076e62b5a535370861
NeoCephei <77801754+NeoCephei@users.noreply.github.com>
2021-11-01 11:15:22 +0100

    feat: pieces done still needs functionallity

--- client/src/Components/gameWrapper/gameContainer/pieces/player1Pieces.js
+++ client/src/Components/gameWrapper/gameContainer/pieces/player1Pieces.js
@@ -4,16 +4,22 @@ import React, {useState}from 'react'
 function Player1Pieces (props) {
 
   const [smallPieces, setSmallPieces] = useState(3);
-  const [medium, setMediumPieces] = useState(3);
+  const [mediumPieces, setMediumPieces] = useState(3);
   const [largePieces, setLargePieces] = useState(3);
 
   return (
     <div className="player">
       <h1>Player 1</h1>
       <div className="pieces">
-        <div className="s-piece"></div>
-        <div className="m-piece"></div>
-        <div className="l-piece"></div>
+        <div>
+          <div className="piece s-piece">x {smallPieces}</div>
+        </div>
+        <div>
+          <div className="piece m-piece">x {mediumPieces}</div>
+        </div>
+        <div>
+          <div className="piece l-piece">x {largePieces}</div>
+        </div>
       </div>
     </div>
   )
--- client/src/Components/gameWrapper/gameContainer/pieces/player2Pieces.js
+++ client/src/Components/gameWrapper/gameContainer/pieces/player2Pieces.js
@@ -1,15 +1,25 @@
 import './pieces.css'
-import React from 'react'
+import React, {useState}from 'react'
 
 function Player2Pieces (props) {
+
+  const [smallPieces, setSmallPieces] = useState(3);
+  const [mediumPieces, setMediumPieces] = useState(3);
+  const [largePieces, setLargePieces] = useState(3);
 
   return (
     <div className="player">
       <h1>Player 2</h1>
       <div className="pieces">
-        <div className="s-piece"></div>
-        <div className="m-piece"></div>
-        <div className="l-piece"></div>
+        <div>
+          <div className="piece s-piece">x {smallPieces}</div>
+        </div>
+        <div>
+          <div className="piece m-piece">x {mediumPieces}</div>
+        </div>
+        <div>
+          <div className="piece l-piece">x {largePieces}</div>
+        </div>
       </div>
     </div>
   )
